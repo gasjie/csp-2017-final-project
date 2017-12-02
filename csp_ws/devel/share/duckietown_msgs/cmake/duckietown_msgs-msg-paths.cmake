56431eb5d09ff9fbd559f3cc67482de243394ad3
w86763777 <w86763777@gmail.com>
2017-12-02 04:11:22 +0000

    resolve dependency of duckietown_msg

---
+++ csp_ws/devel/share/duckietown_msgs/cmake/duckietown_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(duckietown_msgs_MSG_INCLUDE_DIRS "/home/robotvision/csp_project/csp_ws/src/duckietown_msgs/msg")
+set(duckietown_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;sensor_msgs)
